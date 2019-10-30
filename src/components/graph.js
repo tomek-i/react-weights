7a3ce846bb584f2a80e909d7c0b3156dbb321d9a
Tomek I <tomek.iwainski@gmail.com>
2019-10-30 12:07:13 +1100

    increased point radius

--- src/components/graph.js
+++ src/components/graph.js
@@ -24,7 +24,7 @@ function Graph(props){
           pointHoverBackgroundColor: 'rgba(75,192,192,1)',
           pointHoverBorderColor: 'rgba(220,220,220,1)',
           pointHoverBorderWidth: 2,
-          pointRadius: 1,
+          pointRadius: 5,
           pointHitRadius: 10,
           data: props.data,
 
