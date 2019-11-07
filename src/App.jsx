c9221e9df6b64eb1287de9bf20cf280f21eec00a
Tomek I <tomek.iwainski@gmail.com>
2019-11-07 16:50:07 +1100

    added axios

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,20 @@ import './css/App.css';
 import WeightInputForm from './components/form'
 import Graph from './components/graph'
 import Stats from './components/stats'
-
+import axios from 'axios';
 
 function App() {
 
   const [data, setData] = useState([])
 
   useEffect(() => {
+
+    //TODO: get data from BACKEND
+
+    axios.get('localhost:5000/api/data').then(result => {
+      console.log("GET", result);
+    });
+
 
     const items = localStorage.getItem('data');
     if (items) {
@@ -24,21 +31,27 @@ function App() {
   function appendData(item) {
     const date = new Date()
     const datapoint = {
-      "date": date.toLocaleDateString(),
+      "date": date,//.toLocaleDateString(),
       "value": item
     }
+
+    //TODO: POST data to backend
+    axios.post('localhost:5000/api/data', { value: item }).then(result => console.log(result));
+
 
     setData(data.concat(datapoint))
 
   }
-  useEffect(() => {
 
+
+  useEffect(() => {
+    //TODO: probably not required anymore?
     if (data.length > 0) {
 
       const j = JSON.stringify(data)
+
       localStorage.setItem("data", j)
     }
-
 
   }, [data])
 
