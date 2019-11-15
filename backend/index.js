a93c3133d7d9222b3951b1ab51dfa8e75f0a2d84
Tomek I <tomek.iwainski@gmail.com>
2019-11-15 12:24:37 +1100

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,23 @@
-require('dotenv').config();
-const debug = require('debug')('app:init');
-const config = require('config'); //
-const helmet = require('helmet');
-const morgan = require('morgan');
-const express = require('express');
-const cors = require('cors');
+// app.js
 
-//const _ = require('underscore');
+/**
+ * Required External Modules
+ */
+require("dotenv").config();
+const debug = require("debug")("app:init");
+const config = require("config"); //
+const helmet = require("helmet");
+const morgan = require("morgan");
+const express = require("express");
+const cors = require("cors");
 
-const logger = require('./middleware/logger');
-const dataRouter = require('./routes/data');
-const connectDB = require('./db/db'); //connect to db
+/**
+ * App Variables
+ */
 
 const app = express();
 
+<<<<<<< Updated upstream
 //Connect to DB
 connectDB();
 
@@ -37,23 +41,41 @@ var corsOptions = {
     }
   }
 };
+=======
+debug("Debug: " + config.get("debug"));
+debug("App name: " + config.get("name"));
+debug("Mail Server: " + config.get("mail.host"));
+debug("Mail Password: " + config.get("mail.password"));
+>>>>>>> Stashed changes
 
 app.use(cors());
 app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-if (app.get('env') === 'development') {
-  debug('Morgan HTTP logging middleware enabled ...');
-  app.use(morgan('tiny'));
-
-  debug('Custom logging middleware enabled ...');
-  app.use(logger); //custom logger
+if (app.get("env") === "development") {
+  debug("Morgan HTTP logging middleware enabled ...");
+  app.use(morgan("debug"));
 }
 
-app.use('/api/data', dataRouter);
+/**
+ * Database Activation
+ */
+const db = require("./database/db"); //connect to db
+db().catch(err => {
+  debug("Unable to connect to database.", err);
+  process.exit(1);
+});
 
+/**
+ * Routes
+ */
+app.use(require("./routes"));
+
+/**
+ * Server
+ */
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   debug(`listening on port ${port} ...`);
